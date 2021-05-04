18404dc98912e561543faf832e844eaa0961ef4f
DSzhongweizi <DSzhongweizi@gmail.com>
2021-05-04 21:54:38 +0800

    a

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下 
+cd dist
+
+echo 'www.dengsong.icu' >> CNAME
+echo "个人博客：http://blog.dengsong.icu" >> README.md
+
+git init
+git add .
+git commit -m "deploy"
+git branch -M main
+git remote add origin git@github.com:DSzhongweizi/DSzhongweizi.github.io.git
+git pull origin main --allow-unrelated-histories
+git push -u origin main
+
+cd -
