b828ea3cb19750aff868157d64ab07bb59973caa
DSzhongweizi <1487907946@qq.com>
2021-11-11 18:05:33 +0800

    环境id

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cloudbase = require("@cloudbase/node-sdk");
 const app = express();
-const db = cloudbase.init({ env: "blog" }).database();
+const db = cloudbase.init({ env: "blog-4gr30cdu25d98dfd" }).database()
 
 app.get("/", (req, res) => {
   res.send("result");
 });
 app.post("/save-article", (req, res) => {
-  db.collection("articles").add({ name: "luke-111111" }).then(result => {
+  db.collection("articles").add({ name: "ds" }).then(result => {
     res.send(result);
   })
 });
