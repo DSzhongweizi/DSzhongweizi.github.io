d9155698015194d71c9046862ac56cf60f6cfa55
DSzhongweizi <1487907946@qq.com>
2021-11-11 16:06:44 +0800

    post 测试

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const cloudbase = require("@cloudbase/node-sdk");
 const app = express();
 const db = cloudbase.init({ env: "blog" }).database();
 
-app.get("/save-article", (req, res) => {
-  db.collection("articles").add({ _openid: "luke-111111" }).then(result => {
-    res.send(result.id);
+app.get("/", (req, res) => {
+  res.send("result");
+});
+app.post("/save-article", (req, res) => {
+  db.collection("articles").add({ name: "luke-111111" }).then(result => {
+    res.send(result);
   })
 });
+
 app.listen(8080);
 
 
